374dad0341fa7b586182ae66c40827770a37dd47
shiyingyu <shiying.yu@gmail.com>
2021-12-12 22:45:36 +0800

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,26 @@ void setup() {
     Serial.println("Failed to initialize ADS.");
     while (1);
   }
+  // 重要，设置为GAIN_TWO可得到较高的分辨率，不可乱改，跟电路设计有关，若超出范围，会导致读取值错误甚至烧坏芯片
   ads.setGain(GAIN_TWO);
 }
 
 void loop() {
-  int16_t adc0, adc1;
-  float volts0, volts1, v;
+  int16_t adc0;
+  float volts0, v;
+  // 读取第一个通道的采样值
   adc0 = ads.readADC_SingleEnded(0);
+  // 转换为电压（单位V）
   volts0 = ads.computeVolts(adc0);
-  // 标定系数，每个板子不一样
+
+  // 根据电路板的体质各异进行标定，获取更为精准的电压值
+  // 标定系数
   float k = 1.59064, b = 1.00513;
+
+  // 标定公式，将ADC值转换为真实的电压值
   v = (volts0 - b) / k - ((volts0 < 1.0052) ? 0.0012 : 0);
 
-  Serial.print("AIN0: "); Serial.print(adc0); Serial.print("  "); Serial.print(volts0, 4); Serial.print("V   ");Serial.print(v, 4); Serial.println("V");
+  Serial.print(v, 4); Serial.println("V");
 
   delay(100);
 }
